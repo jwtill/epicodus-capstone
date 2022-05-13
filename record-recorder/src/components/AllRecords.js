0ab35dcc6a672433643a8d021b5715fd94567bf6
Josh Tillinghast <jwtill@icloud.com>
2022-05-13 16:27:05 -0700

    Add styles for record list

--- record-recorder/src/components/AllRecords.js
+++ record-recorder/src/components/AllRecords.js
@@ -4,19 +4,19 @@ import Record from "./Record"
 export default function AllRecords(props) {
   return (
     <div>
-      <ul>
+      <ul className="record-list">
         {props.records.map((record) =>
-          <li>
+    
             <Record
               title={record.title}
-              artist={record.title}
-              genre={record.title}
-              year={record.title}
-              format={record.title}
-              value={record.title}
-              condition={record.title}
+              artist={record.artist}
+              genre={record.genre}
+              year={record.year}
+              format={record.format}
+              value={record.value}
+              condition={record.condition}
             />
-          </li>
+         
         )}
         </ul>
     </div>
--- record-recorder/src/components/Record.js
+++ record-recorder/src/components/Record.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import AllRecords from "./AllRecords"
+
 
 function Record(props) {
   return (
