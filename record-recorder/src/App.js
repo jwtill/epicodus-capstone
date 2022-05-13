2aad846b5472dda10df0251fb52c83a7cd536384
Josh Tillinghast <jwtill@icloud.com>
2022-05-13 08:13:59 -0700

    Update App.js

--- record-recorder/src/App.js
+++ record-recorder/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Header from "./components/Header"
+import Form from "./components/Form"
 
-function App() {
+export default function App() {
+  const [selectedMenu, setSelectedMenu] = React.useState('Home');
+
+  const handleChange = (event) => {
+    console.log(selectedMenu)
+    setSelectedMenu(event.target.className);
+  };
+ 
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header selectedMenu={selectedMenu} handleChange={handleChange}/>
+      {selectedMenu === "Add a Record" && <Form />}
     </div>
+
   );
 }
 
-export default App;
+
