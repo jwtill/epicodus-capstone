7fa3ada2f346cb3fb05b6089e4a7100f5e316f1e
Josh Tillinghast <jwtill@icloud.com>
2022-05-13 12:01:17 -0700

    Work on saving state for record form

--- record-recorder/src/App.js
+++ record-recorder/src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Header from "./components/Header"
 import Form from "./components/Form"
+import { nanoid } from "nanoid"
 
 export default function App() {
   const [selectedMenu, setSelectedMenu] = React.useState('Home');
@@ -9,11 +10,43 @@ export default function App() {
     setSelectedMenu(event.target.className);
     console.log(selectedMenu)
   }
+  const [records, setRecords] = React.useState(
+    () => JSON.parse(localStorage.getItem("records")) || []
+  )
+  const [currentRecordId, setCurrentRecordId] = React.useState(
+    (records[0] && records[0].id) || ""
+  )
+
+  console.log(records);
+
+  React.useEffect(() => {
+    localStorage.setItem("records", JSON.stringify(records))
+  }, [records]) //when 'records' is update, do this
+
+  handleAddingNewRecordToList = (newRecord) => {
+    const newMainRecordList = this.state.mainRecordList.concat(newRecord);
+    this.setState({
+      mainKegList: newMainKegList,
+      formVisibleOnPage: false
+    });
+  }
+  // function createNewNote() {
+  //   const newRecord = {
+  //     id: nanoid(),
+  //     body: "# Type your markdown note's title here"
+  //   }
+  //   setRecords(prevRecords => [newRecord, ...prevRecords])
+  //   setCurrentRecordId(newRecord.id)
+  // }
+
+ 
+
 
   return (
     <div>
       <Header selectedMenu={selectedMenu} handleChange={handleChange} />
-      {selectedMenu === "Add a Record" && <Form />}
+      {selectedMenu === "Add a Record" && 
+      <Form onNewRecordCreation={this.handleAddingNewRecordToList}/>}
     </div>
 
   );
