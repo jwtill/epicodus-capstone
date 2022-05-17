097cb4a6697ee4d530f8f27877c542cce8cf3884
Josh Tillinghast <jwtill@icloud.com>
2022-05-17 12:10:53 -0700

    WIP troubleshoot adding firebase

--- record-recorder/src/index.js
+++ record-recorder/src/index.js
@@ -2,11 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css';
 import App from './App';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+import firebase from "./firebase";
+
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/index';
+import { createStore } from 'redux';
+
+
+const store = createStore(rootReducer);
+
+const rrfProps = {
+  firebase,
+  config: {
+    userProfile: "users"
+  },
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
+  </Provider>
+  </React.StrictMode >
 );
 
